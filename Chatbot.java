24d8af6e318373062dc0264269aba05304e05df0
vk <nineteeeeeeen@gmail.com>
2021-12-30 18:08:35 +0300

    Chatbot 5 stage

--- Chatbot.java
+++ Chatbot.java
@@ -15,9 +15,7 @@ public class ChatBot {
         var num2 = scanner.nextInt();
         var num3 = scanner.nextInt();
         int age = (num1 * 70 + num2 * 21 + num3 * 15) % 105;
-
         System.out.println("You age is " + age + "; " + "that's a good time to start programming!");
-
         System.out.println("Now i will prove to you that I can count to any number you want");
         Scanner scanner2 = new Scanner(System.in);
         var num4 = scanner.nextInt();
@@ -27,6 +25,24 @@ public class ChatBot {
                 counter += 1;
                 System.out.println(counter + "!");
                 i++;
+            }
+        }
+        String text = "Let's test your programming knowledge \nWhy do we use methods? \n1. To repeat a statement multiple times. " +
+                "\n2. To decompose a program into several small subroutines. \n3. To determine the execution time jf a program." +
+                "\n4. To interrupt te execution of a program";
+        System.out.println(text);
+        while (true) {
+            Scanner scanner3 = new Scanner(System.in);
+            var num5 = scanner.nextInt();
+            if (num5 < 3) {
+                System.out.println("Please, try again");
+                continue;
+            } else if (num5 > 3) {
+                System.out.println("Please, try again");
+            } else if (num5 == 3) {
+                System.out.println("Great, you right!" +
+                        "\nGoodbye, have a nice day!");
+                break;
             }
         }
     }
