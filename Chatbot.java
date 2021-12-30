0f3a2daf4211e7a4040518a369a01f8a2f373ec2
vk <nineteeeeeeen@gmail.com>
2021-12-30 18:04:03 +0300

    Chatbot 2 stage

--- Chatbot.java
+++ Chatbot.java
@@ -1,8 +1,14 @@
 package ChatBot;
+import java.util.Scanner;
 
 public class ChatBot {
     public static void main(String[] args) {
         System.out.println("Hello my name is Aboba.");
         System.out.println("I was created in 2021.");
+
+        System.out.println("Please, remind me your name.");
+        Scanner scanner = new Scanner(System.in);
+        String myName = scanner.next();
+        System.out.println("What a great name you have," + myName + "!");
     }
 }
